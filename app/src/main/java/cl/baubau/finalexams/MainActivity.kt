284e6c6e2a1af61187db7d1ce26cb56ea32d4499
UlisesR2002 <116087176+UlisesR2002@users.noreply.github.com>
2023-12-15 16:09:09 -0300

    Diseño funcional del tab para dificultad de Option Activity (15/12) Ulises Muñoz

--- app/src/main/java/cl/baubau/finalexams/MainActivity.kt
+++ app/src/main/java/cl/baubau/finalexams/MainActivity.kt
@@ -1,14 +1,29 @@
 package cl.baubau.finalexams
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var playButton: Button
+    private lateinit var optionButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        playButton = findViewById(R.id.mainPlayButton)
+        optionButton = findViewById(R.id.mainOptionButton)
+
+        playButton.setOnClickListener{
+            val intentPlayActivity = Intent(this, PlayActivity::class.java)
+            startActivity(intentPlayActivity)
+        }
+
+        optionButton.setOnClickListener{
+            val intentOptionActivity = Intent(this, OptionActivity::class.java)
+            startActivity(intentOptionActivity)
+        }
     }
 }
--- app/src/main/java/cl/baubau/finalexams/OptionActivity.kt
+++ app/src/main/java/cl/baubau/finalexams/OptionActivity.kt
@@ -2,10 +2,38 @@ package cl.baubau.finalexams
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class OptionActivity : AppCompatActivity() {
+    private lateinit var easyDifficultyButton: Button
+    private lateinit var mediumDifficultyButton: Button
+    private lateinit var hardDifficultyButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_option)
+
+        easyDifficultyButton = findViewById(R.id.optionEasyButton)
+        mediumDifficultyButton = findViewById(R.id.optionMediumButton)
+        hardDifficultyButton = findViewById(R.id.optionHardButton)
+
+        easyDifficultyButton.setOnClickListener{
+            selectDifficulty(easyDifficultyButton)
+        }
+        mediumDifficultyButton.setOnClickListener{
+            selectDifficulty(mediumDifficultyButton)
+        }
+        hardDifficultyButton.setOnClickListener{
+            selectDifficulty(hardDifficultyButton)
+        }
+    }
+
+    private fun selectDifficulty(selectedButton: Button) {
+        // Restablecer el color predeterminado para todos los botones
+        easyDifficultyButton.backgroundTintList = getColorStateList(R.color.colorTabButtonBackground)
+        mediumDifficultyButton.backgroundTintList = getColorStateList(R.color.colorTabButtonBackground)
+        hardDifficultyButton.backgroundTintList = getColorStateList(R.color.colorTabButtonBackground)
+
+        // Establecer el color del botón seleccionado
+        selectedButton.backgroundTintList = getColorStateList(R.color.colorTabButtonBackgroundSelected)
     }
 }
