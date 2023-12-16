20b2e4c8a8e55709048703db4ace569f9452ace5
UlisesR2002 <116087176+UlisesR2002@users.noreply.github.com>
2023-12-16 13:13:08 -0300

    Se pueden acceder preferencias entres Activities (16/12) Ulises Muñoz

--- app/src/main/java/cl/baubau/finalexams/OptionActivity.kt
+++ app/src/main/java/cl/baubau/finalexams/OptionActivity.kt
@@ -24,7 +24,12 @@ class OptionActivity : AppCompatActivity() {
         hardDifficultyButton = findViewById(R.id.optionHardButton)
         selectCategoryButton = findViewById(R.id.optionSelectCategoryButton)
 
-        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences("common_prefs", Context.MODE_PRIVATE)
+
+        val (selectedCategories, categoryNames) = loadSelectedCategories(sharedPreferences)
+        println("Category Names: ${categoryNames.joinToString(", ")}")
+        println("Category Bool: ${selectedCategories.joinToString(", ")}")
+
 
         // Obtener la preferencia de dificultad almacenada
         val savedDifficulty = sharedPreferences.getString(KEY_DIFFICULTY, DEFAULT_DIFFICULTY)
@@ -78,43 +83,19 @@ class OptionActivity : AppCompatActivity() {
         // Establecer el color del botón seleccionado
         selectedButton.backgroundTintList = getColorStateList(R.color.colorTabButtonBackgroundSelected)
     }
-    private fun saveSelectedCategories(selectedCategories: BooleanArray) {
+
+    private fun saveSelectedCategories(selectedCategories: BooleanArray, categoryNames: Array<String>) {
+        val selectedCategoryNames = categoryNames.filterIndexed { index, _ ->
+            selectedCategories[index]
+        }.toTypedArray()
+
         with(sharedPreferences.edit()) {
             for (i in 0 until selectedCategories.size) {
-                putBoolean("$KEY_CATEGORY$i", selectedCategories[i])
+                putBoolean("${KEY_CATEGORY}$i", selectedCategories[i])
             }
+            putString(KEY_CATEGORY_NAMES, selectedCategoryNames.joinToString(","))
             apply()
         }
-    }
-
-    private fun loadSelectedCategories(): BooleanArray {
-        val categories = arrayOf(
-            "Any category", "General knowledge", "Books", "Film", "Music", "Musical & Theatres", "Television", "Video games",
-            "Board games", "Science & Nature", "Computers", "Mathematics", "Mythology", "Sports", "Geography", "History",
-            "Politics", "Art", "Celebrities", "Animals", "Vehicles", "Comics", "Gadgets", "Japanese Anime & Manga", "Cartoon & Animations"
-        )
-
-        val selectedCategories = BooleanArray(categories.size) { false }
-
-        for (i in 0 until categories.size) {
-            selectedCategories[i] = sharedPreferences.getBoolean("$KEY_CATEGORY$i", false)
-        }
-
-        // Verificar si es la primera vez que se carga la preferencia
-        val isFirstTime = sharedPreferences.getBoolean("FIRST_TIME_LOAD_CATEGORIES", true)
-
-        if (isFirstTime) {
-            // Establecer "Any category" como activado por defecto solo la primera vez
-            selectedCategories[0] = true
-
-            // Guardar la indicación de que ya no es la primera vez
-            with(sharedPreferences.edit()) {
-                putBoolean("FIRST_TIME_LOAD_CATEGORIES", false)
-                apply()
-            }
-        }
-
-        return selectedCategories
     }
 
     private fun showCategorySelectionDialog() {
@@ -124,24 +105,25 @@ class OptionActivity : AppCompatActivity() {
             "Politics", "Art", "Celebrities", "Animals", "Vehicles", "Comics", "Gadgets", "Japanese Anime & Manga", "Cartoon & Animations"
         )
 
-        val selectedCategories = loadSelectedCategories()
+        val (selectedCategories, _) = loadSelectedCategories(sharedPreferences)
 
         val builder = AlertDialog.Builder(this)
         val dialog: AlertDialog = builder.setTitle(R.string.option_select_category_button)
             .setMultiChoiceItems(categories, selectedCategories) { _, which, isChecked ->
-                selectedCategories[which] = isChecked
-                if (which != 0) {
+                if (which == 0) {
+                    // Si seleccionas "Any Category", desmarcar todas las demás categorías
+                    for (i in 1 until selectedCategories.size) {
+                        selectedCategories[i] = false
+                    }
+                } else {
+                    // Si seleccionas cualquier otra categoría, desmarcar "Any Category"
                     selectedCategories[0] = false
                 }
 
-                for (i in 1 until selectedCategories.size) {
-                    if (selectedCategories[0]) {
-                        selectedCategories[i] = false
-                    }
-                }
+                selectedCategories[which] = isChecked
             }
             .setPositiveButton("Accept") { _, _ ->
-                saveSelectedCategories(selectedCategories)
+                saveSelectedCategories(selectedCategories, categories)
             }
             .setNegativeButton("Cancel") { _, _ ->
                 // Cancelar, no se realiza ninguna acción
@@ -151,12 +133,35 @@ class OptionActivity : AppCompatActivity() {
         dialog.show()
     }
     companion object {
-        private const val KEY_DIFFICULTY = "difficulty_preference"
-        private const val DEFAULT_DIFFICULTY = "easy"
+        const val KEY_DIFFICULTY = "difficulty_preference"
+        const val DEFAULT_DIFFICULTY = "easy"
         private const val DIFFICULTY_EASY = "easy"
         private const val DIFFICULTY_MEDIUM = "medium"
         private const val DIFFICULTY_HARD = "hard"
 
         private const val KEY_CATEGORY = "category_preference"
+        const val KEY_CATEGORY_NAMES = "category_names_preference"
+
+        fun loadSelectedCategories(sharedPreferences: SharedPreferences): Pair<BooleanArray, Array<String>> {
+            val categories = arrayOf(
+                "Any category", "General knowledge", "Books", "Film", "Music", "Musical & Theatres", "Television", "Video games",
+                "Board games", "Science & Nature", "Computers", "Mathematics", "Mythology", "Sports", "Geography", "History",
+                "Politics", "Art", "Celebrities", "Animals", "Vehicles", "Comics", "Gadgets", "Japanese Anime & Manga", "Cartoon & Animations"
+            )
+
+            // Cargar los valores de las categorías desde SharedPreferences
+            val selectedCategories = BooleanArray(categories.size) {
+                sharedPreferences.getBoolean("${KEY_CATEGORY}$it", it == 0) // Establecer "Any category" como true por defecto
+            }
+
+            val namesString = sharedPreferences.getString(KEY_CATEGORY_NAMES, null)
+            val categoryNames = if (!namesString.isNullOrBlank()) {
+                namesString.split(",").toTypedArray()
+            } else {
+                arrayOf("Any category")
+            }
+
+            return Pair(selectedCategories, categoryNames)
+        }
     }
 }
--- app/src/main/java/cl/baubau/finalexams/PlayActivity.kt
+++ app/src/main/java/cl/baubau/finalexams/PlayActivity.kt
@@ -1,11 +1,24 @@
 package cl.baubau.finalexams
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class PlayActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
+
+        sharedPreferences = getSharedPreferences("common_prefs", Context.MODE_PRIVATE)
+
+        val savedDifficulty = sharedPreferences.getString(OptionActivity.KEY_DIFFICULTY, OptionActivity.DEFAULT_DIFFICULTY)
+
+        val (selectedCategories, categoryNames) = OptionActivity.loadSelectedCategories(sharedPreferences)
+
+        println(savedDifficulty)
+        println("Category Names: ${categoryNames.joinToString(", ")}")
+        println("Category Bool: ${selectedCategories.joinToString(", ")}")
     }
 }
